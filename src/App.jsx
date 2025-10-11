64562dd0adea2b19f6be89dd56783fd24eea0235
IAmNotGod-core <lintangksatria1@gmail.com>
2025-10-11 17:28:39 +0700

    refactor: remove unused import from App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { use, useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { useDebounce } from 'react-use'
 import Search from './components/Search.jsx'
 import Spinner from './components/Spinner.jsx'
